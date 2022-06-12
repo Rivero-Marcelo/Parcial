d85e171e8d0c10e819f35798bc9825b9bed528ff
Marcelo Rivero <rivero_marcelo@hotmail.com>
2022-06-12 02:36:17 -0300

    Falta terminar y corregir

---
+++ Controladores/CompraControlador.class.php
@@ -0,0 +1,52 @@
+<?php
+
+require "autoload.php";
+
+
+class CompraControlador {
+
+
+    public static function FormularioCompra(){
+        require_once "Vistas/FormularioCompra.php";
+    }
+
+    
+   public static function Alta($id_persona,$id_producto,$fecha_hora) {
+        
+        $c = new CompraModelo();
+        $c -> id_persona = $id_persona;
+        $c -> id_producto = $id_producto;
+        $c -> fecha_hora = $fecha_hora;
+        $c -> Guardar();
+    }
+
+public static function Lista(){
+    $c = new CompraModelo();
+    $compras = $c -> ObtenerCompras();
+    $listadoDeCompras = self::FormatearResultado($compras);
+    return $listadoDeCompras; 
+}
+
+
+private static function FormatearResultado($compras){
+
+    $listadocompras = array();
+    while($compra = $compras->fetch_assoc()){
+    $persona = $compra['NombrePersona'] . " " . $compra['ApellidoPersona'];
+        
+    $productosEncompra = array(
+
+           'NombreProducto' => $compra["NombreProducto"],
+           'DescProducto' => $compra["DescProducto"]
+        );
+  
+        $listadocompras[$persona][] = $productosEncompra;
+  
+    } 
+
+    return $listadocompras;
+
+}
+
+
+}
